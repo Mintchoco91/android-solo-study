87e3b307a62b8736433bf3caa4a282735f97cc48
DESKTOP-PA044E1\Kiwon <boy0221@nate.com>
2021-10-03 17:56:47 +0900

    안드로이드 앱 만들기 #3 (Intent 화면전환)

--- app/src/main/java/com/example/test3/SubActivity.java
+++ app/src/main/java/com/example/test3/SubActivity.java
@@ -2,13 +2,25 @@ package com.example.test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
+
+    private TextView tv_sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        Intent intent = getIntent();
+        String prevPageStr =  intent.getStringExtra("prevPageStr");
+
+        tv_sub.setText(prevPageStr);
+
     }
 }
