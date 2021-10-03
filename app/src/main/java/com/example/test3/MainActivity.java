81cbf341e0d9c4dc728d7daf2a9b4ae5254aee9d
DESKTOP-PA044E1\Kiwon <boy0221@nate.com>
2021-10-03 21:06:06 +0900

    안드로이드 앱 만들기 #6 (ListView)

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //https://www.youtube.com/watch?v=fRDy13p8L78&list=PLC51MBz7PMyyyR2l4gGBMFMMUfYmBkZxm&index=5
 //안드로이드 앱 만들기 #4 (Intent 화면전환) - 쉽게 앱 만드는 방법 (현직 개발자 설명)
 public class MainActivity extends AppCompatActivity {
 
-    ImageView img_test;
+    private ListView list;
 
     @Override
     //처음 실행할때 실행되는 부분
@@ -23,15 +28,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //객체 가져오기
-        img_test = findViewById(R.id.img_test);
+        list = (ListView) findViewById(R.id.list);
 
-        //버튼 클릭시 액션 주는부분.
-        img_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //1. toast  (getApplicationContext,[내용],[띄울시간])
-                Toast.makeText(getApplicationContext(), "ToastTest",Toast.LENGTH_SHORT).show();
-            }
-        });
+        List<String> data = new ArrayList<>();
+
+        // this, [유형], [어댑터명]
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("data1");
+        data.add("data2");
+        adapter.notifyDataSetChanged(); //이 데이터를 저장해라
     }
 }
